2982c7b79d9cc062eb2a7aa70d923707ce0343a1
Eveline Fotso <Sf656145023pgmail.com>
2025-07-24 16:19:18 +0100

    update autentification and user initialization

--- models/UserModel/UserModel.js
+++ models/UserModel/UserModel.js
@@ -1,14 +1,18 @@
 const db = require("../../config/db");
+const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
 const createTableIfNotExists = async () => {
-  // Vérifie si la table 'users' existe
+  // Vérifie si la table 'users' existe et ajoute les champs role et company
   const checkTableQuery = `
     CREATE TABLE IF NOT EXISTS ${process.env.Table_users} (
       id SERIAL PRIMARY KEY,
+      uid VARCHAR(50) UNIQUE NOT NULL, -- Ajout de l'UID
       name VARCHAR(100) NOT NULL,
       email VARCHAR(100) UNIQUE NOT NULL,
       password VARCHAR(100) NOT NULL,
+      role VARCHAR(50) NOT NULL,
+      company VARCHAR(100),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
   `;
@@ -16,22 +20,23 @@ const createTableIfNotExists = async () => {
 };
 
 const getAllUser = async () => {
-  await createTableIfNotExists(); // Assure-toi que la table existe avant d'interroger
+  await createTableIfNotExists(); // S'assurer que la table existe avant d'interroger
   const { rows } = await db.query(`SELECT * FROM ${process.env.Table_users}`);
   return rows;
 };
 
-const createUser = async (name, email, password) => {
-  await createTableIfNotExists(); // Assure-toi que la table existe avant de créer un utilisateur
+const createUser = async (name, email, password, role, company) => {
+  await createTableIfNotExists();
+  const uid = uuidv4(); // Génère un UID unique
   const { rows } = await db.query(
-    `INSERT INTO ${process.env.Table_users} (name, email, password) VALUES ($1, $2,$3) RETURNING *`,
-    [name, email, password]
+    `INSERT INTO ${process.env.Table_users} (uid, name, email, password, role, company) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+    [uid, name, email, password, role, company]
   );
   return rows[0];
 };
 
 const findUserByEmail = async (email) => {
-  await createTableIfNotExists(); // Assure-toi que la table existe avant de rechercher un utilisateur
+  await createTableIfNotExists(); // S'assurer que la table existe avant de rechercher un utilisateur
   const { rows } = await db.query(
     `SELECT * FROM ${process.env.Table_users} WHERE email = $1`,
     [email]
